29fc19f79136270448949711344c0b62fb802914
Igor Serko <igor.serko@gmail.com>
2015-01-23 01:38:48 +0000

    reverted changes to default_settings

--- resweb/default_settings.py
+++ resweb/default_settings.py
@@ -1,4 +1,3 @@
-import os
-RESWEB_HOST = os.environ.get('RESWEB_HOST') or 'localhost:6379'
-SERVER_HOST = os.environ.get('SERVER_HOST') or '127.0.0.1'
-SERVER_PORT = int(os.environ.get('SERVER_PORT') or '5000')
+RESWEB_HOST = "localhost:6379"
+SERVER_HOST = "127.0.0.1"
+SERVER_PORT = 5000
